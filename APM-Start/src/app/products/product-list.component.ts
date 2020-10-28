f7b5d93fef7a5866657c84cd82532735c14f5ec1
rulewrite <rulewrite@gmail.com>
2020-10-29 00:12:13 +0900

    feat: 이미지 태그 출력 및 바인딩

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
 })
 export class ProductListComponent {
   pageTitle = 'Product List';
+  imageWidth = 50;
+  imageMargin = 2;
   products = [
     {
       productId: 1,
