b231ca06f72f69690503d2db65eba6733cd95a94
rulewrite <rulewrite@gmail.com>
2020-10-29 23:19:23 +0900

    feat: Product 인터페이스 추가

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './Product';
 
 @Component({
   selector: 'pm-products',
@@ -10,7 +11,7 @@ export class ProductListComponent {
   imageMargin = 2;
   showImage = false;
   listFilter = 'cart';
-  products = [
+  products: Product[] = [
     {
       productId: 1,
       productName: 'Leaf Rake',
