d7b0bbc53eddda93e2d7569b1f3796199b620a78
rulewrite <rulewrite@gmail.com>
2020-11-04 02:03:29 +0900

    feat: 제품 데이터 서비스로 분리 및 적용

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from './Product';
+import { ProductService } from './product.service';
 
 @Component({
   selector: 'pm-products',
@@ -24,33 +25,9 @@ export class ProductListComponent implements OnInit {
   }
 
   filteredProducts: Product[];
-  products: Product[] = [
-    {
-      productId: 1,
-      productName: 'Leaf Rake',
-      productCode: 'GDN-0011',
-      releaseDate: 'March 19, 2019',
-      description: 'Leaf rake with 48-inch wooden handle.',
-      price: 19.95,
-      starRating: 3.2,
-      imageUrl: 'assets/images/leaf_rake.png',
-    },
-    {
-      productId: 2,
-      productName: 'Garden Cart',
-      productCode: 'GDN-0023',
-      releaseDate: 'March 18, 2019',
-      description: '15 gallon capacity rolling garden cart',
-      price: 32.99,
-      starRating: 4.2,
-      imageUrl: 'assets/images/garden_cart.png',
-    },
-  ];
+  products: Product[] = [];
 
-  constructor() {
-    this.filteredProducts = this.products;
-    this.listFilter = 'cart';
-  }
+  constructor(private productService: ProductService) {}
 
   onRatingClicked(message: string): void {
     this.pageTitle = `Product List: ${message}`;
@@ -68,6 +45,8 @@ export class ProductListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    console.log('In OnInit');
+    this.products = this.productService.getProducts();
+    this.filteredProducts = this.products;
+    this.listFilter = 'cart';
   }
 }
